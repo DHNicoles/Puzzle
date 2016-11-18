e4aaf30defb7e3749bf4da92cd0d6bacf7464ed9
Nicoles <18800152810@163.com>
2016-11-18 18:44:37 +0800

    2016.11.18-framework

---
+++ Puzzle/Puzzle/content.h
@@ -0,0 +1,17 @@
+/************************************************************************/
+/* the content present in the images                                    */
+/************************************************************************/
+
+#ifndef __CONTENT_H__
+#define __CONTENT_H__
+
+class Content
+{
+public:
+	Content();
+	~Content();
+
+protected:
+	//attributes
+};
+#endif//__CONTENT_H__
