3f012a915e6dd7b9cb38bd18f10aba331756b026
Nicoles <18800152810@163.com>
2016-11-24 16:51:18 +0800

    初级版本

--- Puzzle/Puzzle/main.cpp
+++ Puzzle/Puzzle/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "puzzle.h"
+#include "struct_info.h"
 
-
-
-int main()
+int main(int argc, char* argv[])
 {
+	//cv::Mat img = cv::imread("E:\\workspace\\git\\Puzzle\\Puzzle\\resource//¶þ²ã±³¾°Í¼.png");
+	//cv::imshow("img", img);
+	//cv::waitKey(0);
+	StructInfo structinfo;
+	Puzzle pzl;
+	parseOrDie(argc, argv, structinfo);
+	cv::Mat dst = pzl.Make(structinfo);
 	return 0;
 }
--- Puzzle/Puzzle/puzzle.h
+++ Puzzle/Puzzle/puzzle.h
@@ -4,14 +4,22 @@
 /************************************************************************/
 #ifndef __PUZZLE_H__
 #define __PUZZLE_H__
-
-
+#include "utils.h"
+class StructInfo;
+class ProductImage;
+class Text;
 class Puzzle
 {
 public:
 	Puzzle();
 	~Puzzle();
-	
+	cv::Mat Make(StructInfo& structinfo);
+	void DrawProductImage(cv::Mat& src, std::vector<ProductImage>& productImageVec);
+	void DrawBackground(cv::Mat& src, uchar B, uchar G, uchar R);
+	void DrawText(cv::Mat& src, std::vector<Text>& textVec);
+
+private:
+	std::map<std::string, std::string> font_path_;
 };
 
 #endif//__PUZZLE_H__
